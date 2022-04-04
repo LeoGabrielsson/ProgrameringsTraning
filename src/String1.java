0082fcab607f497dd3f411d12d0a754189198964
TyrkiskPeber <77719459+TyrkiskPeber@users.noreply.github.com>
2022-04-04 11:17:05 +0200

    Initial commit

---
+++ src/String1.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class String1 {
+    public static void main(String[] args) {
+        StringBuilder pre = new StringBuilder(JOptionPane.showInputDialog("Skriv text för att vända"));
+
+        StringBuilder post = pre.reverse();
+
+
+        System.out.println(post.toString());
+    }
+}
---
+++ src/String2.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class String2 {
+    public static void main(String[] args){
+        String noupper = JOptionPane.showInputDialog("Skriv annars hemsök");
+        String upper = "";
+    }
+}
